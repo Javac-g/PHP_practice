6a8784a88e43837368e088f4000ef59c80476ed9
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-08-29 13:18:59 +0300

    Create Exceptions.php

---
+++ Advanced/Exceptions.php
@@ -0,0 +1,63 @@
+<?php
+function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero");
+  }
+  return $dividend / $divisor;
+}
+
+echo divide(5, 0);
+
+try {
+  code that can throw exceptions
+} catch(Exception $e) {
+  code that runs when an exception is caught
+}
+
+  function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero");
+  }
+  return $dividend / $divisor;
+}
+
+try {
+  echo divide(5, 0);
+} catch(Exception $e) {
+  echo "Unable to divide.";
+}
+
+
+  function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero");
+  }
+  return $dividend / $divisor;
+}
+
+try {
+  echo divide(5, 0);
+} catch(Exception $e) {
+  echo "Unable to divide. ";
+} finally {
+  echo "Process complete.";
+}
+
+  function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero", 1);
+  }
+  return $dividend / $divisor;
+}
+
+try {
+  echo divide(5, 0);
+} catch(Exception $ex) {
+  $code = $ex->getCode();
+  $message = $ex->getMessage();
+  $file = $ex->getFile();
+  $line = $ex->getLine();
+  echo "Exception thrown in $file on line $line: [Code $code]
+  $message";
+}
+?>
